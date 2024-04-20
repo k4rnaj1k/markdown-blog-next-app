2d63615e9853d508a713a8ec82f56ed84be2fd56
k4rnaj1k <vladliasota@gmail.com>
2024-04-20 23:15:18 +0300

    Initial pages structure work

---
+++ app/blogs/[blogName]/page.tsx
@@ -0,0 +1,8 @@
+//TODO: replace placeholder with actual page
+export default function BlogPage({ params }: { params: { blogName: string }}) {
+    //blogname
+    //- search for file
+    //- file not found - no luck
+    // file found - render body
+    return <div>This is a template blog page</div>
+}
