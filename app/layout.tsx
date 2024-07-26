e26cff57d2dae75be0569349a9c6a39e6af3cdc7
k4rnaj1k <vladliasota@gmail.com>
2024-07-26 14:58:21 +0300

    Better blockquote styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
 import type { Metadata } from "next";
-import { Inter, Roboto_Mono } from "next/font/google";
+import { Inter, Roboto_Mono, Space_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "./components/Navbar";
 import { unstable_noStore as noStore } from "next/cache";
 
 const inter = Inter({ subsets: ["latin"] });
-
 export const metadata: Metadata = {
   title: "Markdown blog",
   description: "Created with next js",
@@ -14,16 +13,19 @@ export const metadata: Metadata = {
 const roboto = Roboto_Mono({
   subsets: ['latin', 'cyrillic']
 });
+const space = Space_Mono({ subsets: ['latin'], weight: '400', variable: '--space-font' });
+
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   let _ = noStore();
   return (
     <html lang="en" className={roboto.className}>
-      <body>
+      <body className={space.variable}>
         <Navbar />
         {children}
       </body>
