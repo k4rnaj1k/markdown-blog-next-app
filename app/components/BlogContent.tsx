d5aaa042c5ce0c017198a00c0f45fca66aae251f
k4rnaj1k <vladliasota@gmail.com>
2024-09-13 14:02:53 +0300

    Add support for syntax highlight, styling updates

--- app/components/BlogContent.tsx
+++ app/components/BlogContent.tsx
@@ -1,5 +1,31 @@
 import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { nightOwl } from "react-syntax-highlighter/dist/esm/styles/prism";
+
 
 export const BlogContent = ({ children }: { children: string }) => {
-  return <Markdown>{children}</Markdown>
+  return <Markdown
+    remarkPlugins={[remarkGfm]}
+    children={children}
+    components={{
+      code(props) {
+        const { children, className, node, ref: _, ...rest } = props
+        const match = /language-(\w+)/.exec(className || '')
+        return match ? (
+          <SyntaxHighlighter
+            {...rest}
+            PreTag="div"
+            children={String(children).replace(/\n$/, '')}
+            language={match[1]}
+            style={nightOwl}
+          />
+        ) : (
+          <code {...rest} className={className}>
+            {children}
+          </code>
+        )
+      }
+    }}
+  />
 };
